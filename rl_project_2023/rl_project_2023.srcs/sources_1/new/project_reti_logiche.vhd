559c7129a3e618808988773c5d96d7f8fa98f206
Simone Deidier <simone.deidier@mail.polimi.it>
2023-03-08 20:26:01 +0100

    Created Vivado project, created final report

---
+++ rl_project_2023/rl_project_2023.srcs/sources_1/new/project_reti_logiche.vhd
@@ -0,0 +1,60 @@
+----------------------------------------------------------------------------------
+-- Company: Politecnico di Milano
+-- Engineer: D'Alessio Edoardo, Deidier Simone
+-- 
+-- Create Date: 03/08/2023 08:04:07 PM
+-- Design Name: 
+-- Module Name: project_reti_logiche - Behavioral
+-- Project Name: Progetto (Prova Finale) Reti Logiche AA 2022/23
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity project_reti_logiche is
+
+    port(
+        i_clk : in std_logic;
+        i_rst : in std_logic;
+        i_start : in std_logic;
+        i_w : in std_logic;
+        
+        o_z0 : out std_logic_vector(7 downto 0);
+        o_z1 : out std_logic_vector(7 downto 0);
+        o_z2 : out std_logic_vector(7 downto 0);
+        o_z3 : out std_logic_vector(7 downto 0);
+        o_done : out std_logic;
+        
+        o_mem_addr : out std_logic_vector(15 downto 0);
+        i_mem_data : in std_logic_vector(7 downto 0);
+        o_mem_we : out std_logic;
+        o_mem_en : out std_logic
+    );
+
+end project_reti_logiche;
+
+architecture project_reti_logiche_arch of project_reti_logiche is
+
+begin
+
+end project_reti_logiche_arch;
